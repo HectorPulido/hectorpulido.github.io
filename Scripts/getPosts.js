cf2c6e36b04ea39d75c1348e1a89739d0bb727c2
HectorPulido <hectorandrespp@gmail.com>
2018-09-15 20:19:38 -0500

    Update

--- Scripts/getPosts.js
+++ Scripts/getPosts.js
@@ -39,7 +39,7 @@ client.get('https://api.github.com/repos/'+username+'/'+repository+'/contents/' 
                 data = JSON.parse(d);
                 console.log(data);
             });
-            posts.innerHTML += postBase.replace("{Titulo}",data.title).replace("{Contenido}", data.Description).replace("{Link}", url);
+            posts.innerHTML += postBase.replace("{Titulo}",data.Title).replace("{Contenido}", data.Description).replace("{Link}", url);
     
         }
     });
